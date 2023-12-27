f436dfb7d8cacb5bc3755e2cb31897c27b70c249
mariano <mariano_lorenzo@msn.com>
2023-12-27 18:58:01 -0300

    Initial commit

---
+++ app/src/main/java/com/app/olders_rrss/Clases/clsActividad.kt
@@ -0,0 +1,18 @@
+package com.app.am_rrss.Clases
+
+import com.google.android.material.color.utilities.Variant
+import java.util.Date
+
+class clsActividad(
+    strDenominacion: String,
+    strCodigo: String,
+    datInicio: Date,
+    datModif: Date,
+    datResultado: Double,
+    datAvance: Double,
+    booLogro: Boolean,
+    booFinalizado: Boolean,
+    lstLogros: List<String>,
+    lstHistorial: List<Variant>,
+    ) {
+}
